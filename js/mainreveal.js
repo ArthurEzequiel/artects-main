13b4102706d57877c39d3512bb74e029bde71cb6
ArthurSantos <98977659+ArthurEzequiel@users.noreply.github.com>
2024-03-13 06:51:07 -0300

    upd

---
+++ js/mainreveal.js
@@ -0,0 +1,94 @@
+var slideUp = {
+    distance: '15%',
+    origin: 'bottom',
+    delay: 1,
+    duration: 2000,
+    reset: false,
+};
+
+
+ScrollReveal().reveal('.h1-large', slideUp);
+
+
+ScrollReveal().reveal('.slide-left', {
+    delay: 0,
+    duration: 1500,
+    reset: false,
+    origin: 'left',
+    distance: '20%',
+});
+
+
+
+ScrollReveal().reveal('.slide-top-1000', {
+    delay: 1000,
+    duration: 1500,
+    reset: false,
+    origin: 'top',
+    distance: '50%',
+});
+
+ScrollReveal().reveal('.slide-top-500', {
+    delay: 300,
+    duration: 2000,
+    reset: false,
+    origin: 'top',
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.slide-reveal', {
+    delay: 100,
+    duration: 4000,
+    reset: false,
+    origin: 'top',
+    distance: '',
+});
+
+
+ScrollReveal().reveal('.slide-top-100', {
+    delay: 300,
+    duration: 1500,
+    reset: false,
+    origin: 'top',
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.slide-top-200', {
+    delay: 200,
+    duration: 1500,
+    reset: false,
+    origin: 'top',
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.slide-top-300', {
+    delay: 300,
+    duration: 1500,
+    reset: false,
+    origin: 'top',
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.slide-top-400', {
+    delay: 400,
+    duration: 1500,
+    reset: false,
+    origin: 'top',
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.slide-top-600', {
+    delay: 600,
+    duration: 1500,
+    reset: false,
+    origin: 'top',
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.slide-top-700', {
+    delay: 700,
+    duration: 1500,
+    reset: false,
+    origin: 'top',
+    distance: '10%',
+});
